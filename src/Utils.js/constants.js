7ee26aab66b3fd6a4616bed18a29221440865fc5
RAMESH <rmshvalle@gmail.com>
2025-10-18 23:35:23 +0530

    Configured BASE_URL for Render backend

--- src/Utils.js/constants.js
+++ src/Utils.js/constants.js
@@ -1,7 +1,8 @@
 export const BASE_URL =
-    location.hostname === "localhost"
-        ? "http://localhost:5123"
-        : "https://devtinder-web-3yd2.onrender.com";
+  location.hostname === "localhost"
+    ? "http://localhost:5123"
+    : "https://devtinder-api-backend.onrender.com";
+
 
 /*
 export const BASE_URL = 
