acf03ae12be4400f61c69c3e7b0447a798809c6f
RAMESH <rmshvalle@gmail.com>
2025-10-18 19:09:03 +0530

    Updated API URL for live backend

--- src/Utils.js/constants.js
+++ src/Utils.js/constants.js
@@ -1,2 +1,9 @@
+export const BASE_URL =
+    location.hostname === "localhost"
+        ? "http://localhost:5123"
+        : "https://devtinder-api-backend.onrender.com";
+
+/*
 export const BASE_URL = 
     location.hostname === "localhost" ? "http://localhost:5123" : "/api";
+*/
