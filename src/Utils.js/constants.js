20ae42ba0ff45ac2698cf07aa9f224d91f007cbc
RAMESH <rmshvalle@gmail.com>
2025-11-25 18:16:10 +0530

    Update for deployment

--- src/Utils.js/constants.js
+++ src/Utils.js/constants.js
@@ -1,3 +1,5 @@
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
+
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:5123" : "/api";
+  location.hostname === "localhost" ? "http://localhost:5123" : API_BASE;
 
